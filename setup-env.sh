17d2c568d5120b9fd242ec158fc8fdb52b8c4517
John Storey <john@johnstorey.org>
2020-10-11 21:01:13 +0000

    MesmerEnvironement create, save, update tests.

---
+++ setup-env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+go get go.mongodb.org/mongo-driver/mongo 
+go get go.mongodb.org/mongo-driver/mongo/options
+go get go.mongodb.org/mongo-driver/mongo/readpref
+	go get github.com/onsi/ginkgo
+	 go get github.com/onsi/gomega
